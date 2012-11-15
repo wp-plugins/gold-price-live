c982440134ae0d82788a1c69af72af7335baf869
GoldPriceLive <GoldPriceLive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-15 22:06:33 +0000

    Updated Version:
    git-svn-id: https://plugins.svn.wordpress.org/gold-price-live/trunk@625896 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gold-price-live.php
+++ gold-price-live.php
@@ -3,7 +3,7 @@
 Plugin Name: Gold Feed Live
 Plugin URI: http://wordpress.org/extend/plugins/gold-price-live
 Description: Easily place the spot gold price on your blog or website using shortcode. The price is per troy ounce and is in USD. Price output is never more than $0.07 different than the Kitco bid price of gold. <strong>After activating please visit the Tools > Gold Price Live</strong> update options and enable. If you have any questions or require assistance email <a href="mailto:info@gold-feed.com">info@gold-feed.com</a>.
-Version: 1.10
+Version: 2.0
 Author: Gold Feed Inc.
 Author URI: https://gold-feed.com
 */
